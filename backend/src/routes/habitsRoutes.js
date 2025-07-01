d27743d7d153e464b89131672ddd46a2560b7400
Bill Yang <31521678+oddbullet@users.noreply.github.com>
2025-06-30 23:12:10 -0400

    Add habits controller and routes

---
+++ backend/src/routes/habitsRoutes.js
@@ -0,0 +1,18 @@
+import express from "express";
+import {
+  getAllHabits,
+  createHabit,
+  updateHabitTitle,
+  updateHabitCompleteDate,
+  deleteHabit,
+} from "../controllers/habitsController.js";
+
+const router = express.Router();
+
+router.get("/", getAllHabits);
+router.post("/", createHabit);
+router.put("/title/:id", updateHabitTitle);
+router.put("/date/:id", updateHabitCompleteDate);
+router.delete("/:id", deleteHabit);
+
+export default router;
