ad7833044c4c6be17e0768d8475ac9a9a74406a1
Bill Yang <31521678+oddbullet@users.noreply.github.com>
2025-07-24 21:48:22 -0400

    Update HabitPage.jsx

--- frontend/src/pages/HabitPage.jsx
+++ frontend/src/pages/HabitPage.jsx
@@ -2,17 +2,17 @@ import { Button } from "@radix-ui/themes";
 import HeaderBar from "../components/HeaderBar";
 import Habit from "../components/Habit";
 
-export default function HabitPage() {
-  const Week = Object.freeze({
-    SUNDAY: 0,
-    MONDAY: 1,
-    TUESDAY: 2,
-    WEDNESDAY: 3,
-    THURSDAY: 4,
-    FRIDAY: 5,
-    SATURDAY: 6,
-  });
+const Week = Object.freeze({
+  SUNDAY: 0,
+  MONDAY: 1,
+  TUESDAY: 2,
+  WEDNESDAY: 3,
+  THURSDAY: 4,
+  FRIDAY: 5,
+  SATURDAY: 6,
+});
 
+export default function HabitPage() {
   return (
     <div className="habit-page">
       <HeaderBar />
