2c4876b47aec9fe4025c8ebf66d251c1cfd160e2
Bill Yang <31521678+oddbullet@users.noreply.github.com>
2025-08-05 00:44:51 -0400

    remove strictmode

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,18 +5,19 @@ import { BrowserRouter } from "react-router";
 import { Theme } from "@radix-ui/themes";
 import "@radix-ui/themes/styles.css";
 import "./index.css";
+
 import App from "./App.jsx";
 
 import { store } from "./app/store.js";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <Theme radius="small">
-          <App />
-        </Theme>
-      </BrowserRouter>
-    </Provider>
-  </StrictMode>
+  // <StrictMode>
+  <Provider store={store}>
+    <BrowserRouter>
+      <Theme radius="small">
+        <App />
+      </Theme>
+    </BrowserRouter>
+  </Provider>
+  // </StrictMode>
 );
